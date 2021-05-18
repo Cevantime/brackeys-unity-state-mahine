fd69b2cdbd7a2bcc84ef9b950b7ade8b00e4a292
Cevantime <thibaulttruffert@hotmail.com>
2021-05-18 19:04:00 +0200

    start

--- 2D Animation/Assets/Scripts/CharacterBehaviour.cs
+++ 2D Animation/Assets/Scripts/CharacterBehaviour.cs
@@ -4,49 +4,33 @@ using UnityEngine;
 
 public class CharacterBehaviour : MonoBehaviour
 {
-    public float jumpForce = 400f;                            // Amount of force added when the player jumps.
-    [Range(5, 40)] public float crouchSpeed = 17;            // Amount of maxSpeed applied to crouching movement. 1 = 100%
-    [Range(0, .3f)] public float movementSmoothing = .05f;    // How much to smooth out the movement
+    public float jumpForce = 700f;
+    [Range(5, 40)] public float crouchSpeed = 17;
+    [Range(0, .3f)] public float movementSmoothing = .05f;
     [Range(20, 60)] public float runSpeed = 40f;
-    public bool airControl = false;                           // Whether or not a player can steer while jumping;
-    public LayerMask whatIsGround;                            // A mask determining what is ground to the character
-    public Transform groundCheck;                         // A position marking where to check if the player is grounded.
-    public Transform ceilingCheck;                            // A position marking where to check for ceilings
+    public bool airControl = false;
+    public LayerMask whatIsGround;
+    public Transform groundCheck;
+    public Transform ceilingCheck;
     public Collider2D crouchDisableCollider;
+    public const float groundedRadius = .2f;
+    public const float ceilingRadius = .2f;
     public Animator animator;
-
-
-    public const float groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
-    public const float ceilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     public Rigidbody2D body;
-    private StateMachine stateMachine;
 
-    public float horizontalMove = 0.0f;
 
-    public State running;
-    public State jumping;
-    public State crouching;
-
-    void Awake()
-    {
-        body = GetComponent<Rigidbody2D>();
-        stateMachine = new StateMachine();
-        running = new Running(stateMachine, this);
-        jumping = new Jumping(stateMachine, this);
-        crouching = new Crouching(stateMachine, this);
-        stateMachine.ChangeState(running);
-    }
+    [HideInInspector] public float horizontalMove = 0.0f;
 
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal");
-        stateMachine.currentState.LogicUpdate(Time.deltaTime);
+
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
     }
 
     void FixedUpdate()
     {
-        stateMachine.currentState.PhysicUpdate(Time.fixedDeltaTime);
+
         Vector3 localScale = transform.localScale;
         if (body.velocity.magnitude > 0.1f)
         {
